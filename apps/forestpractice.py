bdaa196ca8164a63e1c78c3215b1825f60f5df35
Cristian-Aquino <yojar.aquino@unmsm.edu.pe>
2022-01-28 14:12:14 -0500

    r

--- apps/forestpractice.py
+++ apps/forestpractice.py
@@ -85,4 +85,7 @@ def app():
         #             np.float_(low.title()),
         #             np.float_(close.title())]
         predictS = modelo.predict([[np.float_(open.title()),np.float_(high.title()),np.float_(low.title()),np.float_(close.title())]])
-        st.success('EL CULTIVO RECOMENDADO ES: {}'.format(predictS).upper())
+        if predictS == 1:
+            st.success('COMPRAR:')
+        else:
+            st.success('VENDER')
