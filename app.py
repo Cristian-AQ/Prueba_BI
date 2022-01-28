f8c1fe8b782b579e0bf868658be9b74b807e443a
Cristian-Aquino <yojar.aquino@unmsm.edu.pe>
2022-01-27 22:57:20 -0500

    r

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import forestpractice, modeloNew # import your app modules here
+from apps import forestpractice # import your app modules here
 
 app = MultiApp()
 
 app.add_app("RandomForestClassifier", forestpractice.app)
-app.add_app("ModeloNew", forestpractice.app)
-
 # The main app
 app.run()
