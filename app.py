1bf64de6e3f58097e3af748de4e9244634c55883
Cristian-Aquino <yojar.aquino@unmsm.edu.pe>
2022-01-27 20:10:28 -0500

    r

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import forestpractice # import your app modules here
+from apps import forestpractice,home # import your app modules here
 
 app = MultiApp()
-
-st.markdown("""
-#  Inteligencia de Negocios - Grupo 6
-""")
-
+app.add_app("Home", home.app)
 app.add_app("RandomForestClassifier", forestpractice.app)
 # The main app
 app.run()
