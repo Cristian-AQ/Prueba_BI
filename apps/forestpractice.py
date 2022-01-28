f8bb8ad048b988f21076bbd10f5e0e497598dd4a
Cristian-Aquino <yojar.aquino@unmsm.edu.pe>
2022-01-27 19:59:10 -0500

    r

--- apps/forestpractice.py
+++ apps/forestpractice.py
@@ -68,10 +68,10 @@ def app():
     #Visualizaciones 
     pred_modelo = modelo.predict(X_test)
     st.subheader('Classification Report')
-    st.table(classification_report(y_test,pred_modelo))
+    st.dataframe(classification_report(y_test,pred_modelo))
     st.subheader('Confusion Matrix')
     fig = plt.figure(figsize = (12,6))
-    plot_confusion_matrix(modelo,X_test,y_test)
+    st.pyplot(plot_confusion_matrix(modelo,X_test,y_test))
     st.pyplot(fig)
 
     st.subheader('ROC')
