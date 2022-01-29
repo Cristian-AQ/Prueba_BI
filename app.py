aea07917c0c2afa07be19304b91d5666e3558049
frankpomiano19 <49346525+frankpomiano19@users.noreply.github.com>
2022-01-28 22:34:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from multiapp import MultiApp
 from apps import forestpractice # import your app modules here
 from apps import SVR
 # from apps import KNN
-from apps import LSMT
+#from apps import LSMT
 from apps import RLogistica
 # from apps import SVC
 # from apps import NewModel
@@ -13,7 +13,7 @@ app = MultiApp()
 app.add_app("RANDOM FOREST CLASSIFIER", forestpractice.app)
 app.add_app("SUPPORT VECTOR REGRESION", SVR.app)
 # app.add_app("KNN", KNN.app)
-app.add_app("LSMT", LSMT.app)
+#app.add_app("LSMT", LSMT.app)
 app.add_app("REGRESION LOGISTICA", RLogistica.app)
 # app.add_app("SVC", SVC.app)
 # app.add_app("PROPHET", NewModel.app)
