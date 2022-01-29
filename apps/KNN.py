385403f7dfad55be130ed4d37e834e5f0cdd6884
Jon <60890246+warevil@users.noreply.github.com>
2022-01-29 07:44:03 -0500

    Update KNN.py

--- apps/KNN.py
+++ apps/KNN.py
@@ -32,9 +32,12 @@ def app():
     st.subheader('Datos del 2021 de Diciembre') 
     st.write(df.head())
     
+    df.reset_index(inplace=True)
+    st.write(df.describe())
+    
     # Predictor variables
-    df['Open-Close']= df.Open -df.Close
-    df['High-Low']  = df.High - df.Low
+    df['Open-Close'] = df.Open - df.Close
+    df['High-Low'] = df.High - df.Low
     df =df.dropna()
     X= df[['Open-Close', 'High-Low']]
     st.write(X.head())
@@ -62,8 +65,8 @@ def app():
     accuracy_train = accuracy_score(Y_train, knn.predict(X_train))
     accuracy_test = accuracy_score(Y_test, knn.predict(X_test))
     
-    st.subheader('Train_data Accuracy: %.2f' %accuracy_train)
-    st.subheader('Test_data Accuracy: %.2f' %accuracy_test)
+    st.success('Precisión de datos de entrenamiento: %.2f' %accuracy_train)
+    st.success('Precisión de datos de prueba: %.2f' %accuracy_test)
 
     # Predicted Signal
     df['Predicted_Signal'] = knn.predict(X)
@@ -79,72 +82,13 @@ def app():
     # Plot the results to visualize the performance
     st.subheader('Rendimiento')
     plt.figure(figsize=(10,5))
-    plt.plot(Cumulative_SPY_returns, color='r',label = 'SPY Returns')
-    plt.plot(Cumulative_Strategy_returns, color='g', label = 'Strategy Returns')
+    plt.plot(Cumulative_SPY_returns, color='r',label = 'Retorno de acciones por año')
+    plt.plot(Cumulative_Strategy_returns, color='g', label = 'Retorno de estrategia')
     plt.legend()
     st.pyplot()
     
     Std = Cumulative_Strategy_returns.std()
     Sharpe = (Cumulative_Strategy_returns-Cumulative_SPY_returns)/Std
     Sharpe = Sharpe.mean()
-    st.subheader('Sharpe ratio: %.2f'%Sharpe)
-    
-    # #setting index as date
-    # df['Date'] = pd.to_datetime(df.Date,format='%Y-%m-%d')
-    # df.index = df['Date']
-
-    # st.subheader('Datos del 2021 de Diciembre') 
-    # st.write(df.head())
-    # st.subheader('Historial del precio de cierre') 
-    # st.write(df['Close'])
-    
-    # #creating dataframe with date and the target variable
-    # data = df.sort_index(ascending=True, axis=0)
-    # new_data = pd.DataFrame(index=range(0,len(df)),columns=['Date', 'Close'])
-
-    # for i in range(0,len(data)):
-    #     new_data['Date'][i] = data['Date'][i]
-    #     new_data['Close'][i] = data['Close'][i]
-        
-    # add_datepart(new_data, 'Date')
-    # new_data.drop('Elapsed', axis=1, inplace=True)  #elapsed will be the time stamp
-
-    # #split into train and validation
-    # train = new_data[:987]
-    # valid = new_data[987:]
-
-    # x_train = train.drop('Close', axis=1)
-    # y_train = train['Close']
-    # x_valid = valid.drop('Close', axis=1)
-    # y_valid = valid['Close']
-
-    # #scaling data
-    # scaler = MinMaxScaler(feature_range=(0, 1))
-    # x_train_scaled = scaler.fit_transform(x_train)
-    # x_train = pd.DataFrame(x_train_scaled)
-    # x_valid_scaled = scaler.fit_transform(x_valid)
-    # x_valid = pd.DataFrame(x_valid_scaled)
-
-    # #using gridsearch to find the best parameter
-    # params = {'n_neighbors':[2,3,4,5,6,7,8,9]}
-    # knn = neighbors.KNeighborsRegressor()
-    # model = GridSearchCV(knn, params, cv=5)
-
-    # #fit the model and make predictions
-    # model.fit(x_train,y_train)
-    # preds = model.predict(x_valid)
-    # #rmse
-    # rms=np.sqrt(np.mean(np.power((np.array(y_valid)-np.array(preds)),2)))
-    
-    # st.subheader(f'RMSE: {rms}')
-    
-    # pd.options.mode.chained_assignment = None
-
-    # #plot
-    # valid['Predictions'] = 0
-    # valid['Predictions'] = preds
-    # # st.subheader('Datos del 2021 de Diciembre') 
-    # st.write(valid[['Close', 'Predictions']])
-    # st.write(train['Close'])
-    
+    st.success('Porcentaje de retorno: %.2f'%Sharpe)
     
